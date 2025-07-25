ed4fc7fd33dc1635f6bba227294ca55e5511440e
thusheri <thusheri2003@gmail.com>
2025-07-25 10:42:33 +0530

    import plotly as px

--- App.py
+++ App.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 from io import StringIO
 import numpy as np
-import plotly.express as px
 import plotly.graph_objects as go 
 from plotly.subplots import make_subplots
 
